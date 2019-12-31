6787bc1f63ba9fd01e6a5b1b9d377b3615f2b48d
lxj <14889383@qq.com>
2019-12-31 09:59:47 +0800

    hello

--- NameSpace/namespace.go
+++ NameSpace/namespace.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	cmd := exec.Command ("sh")   //用来指定被fork出来的新进程内的初始化命令，默认来时用sh来执行。
+	cmd := exec.Command("sh")   //用来指定被fork出来的新进程内的初始化命令，默认来时用sh来执行。
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
 	}
